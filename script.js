4a96ce4a3a5db29cfc264f1ffcd6c11431d49ee0
pujitha466 <147580787+pujitha466@users.noreply.github.com>
2024-06-09 19:13:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// Change navigation menu style on scroll
+window.addEventListener('scroll', function() {
+    var navbar = document.getElementById('navbar');
+    if (window.scrollY > 50) {
+      navbar.style.backgroundColor = '#222';
+    } else {
+      navbar.style.backgroundColor = '#333';
+    }
+  });
+  
